46a769bfe64caa09c7b41cbe90c51a51f6a06c81
Yiyoung Liu <y-liu20@ti.com>
2026-06-10 15:47:22 -0500

    main: implement option parsing and overall process
    Parse custom options for config and plugin locations, and deal with the
    core logic of the program.
    
    Signed-off-by: Yiyoung Liu <y-liu20@ti.com>

---
+++ framework/core/main.c
@@ -0,0 +1,43 @@
+#include <getopt.h>
+
+#include "pipeline.h"
+
+int main(int argc, char *argv[]) {
+	const char *config_path = NULL;
+	const char *plugin_dir = NULL;
+
+	static const struct option opts[] = {
+		{ "config", required_argument, NULL, 'c' },
+		{ "plugin-dir", required_argument, NULL, 'p' },
+		{ NULL, 0, NULL, 0 }
+	};
+
+	int c;
+	while ((c = getopt_long(argc, argv, "", opts, NULL)) != -1) {
+		switch (c) {
+		case 'c':
+			config_path = optarg;
+			break;
+		case 'p':
+			plugin_dir = optarg;
+			break;
+		default:
+			fprintf(stderr, "Usage: %s [--config=PATH] [--plugin-dir=DIR]\n", argv[0]);
+			return 1;
+		}
+	}
+
+	if (!config_path || !plugin_dir) {
+		fprintf(stderr, "Usage: %s [--config=PATH] [--plugin-dir=DIR]\n", argv[0]);
+		return 1;
+	}
+
+	struct pipeline *pl = pipeline_create(config_path, plugin_dir);
+	if (!pl) {
+		fprintf(stderr, "Failed to create pipeline!\n");
+		return 1;
+	}
+	pipeline_run(pl);
+	pipeline_destroy(pl);
+	return 0;
+}
