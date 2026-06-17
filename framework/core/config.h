950dbf7c1930a4a8521ec5f104f594378ae900f6
Yiyoung Liu <y-liu20@ti.com>
2026-06-17 08:58:36 -0500

    config: add typed parameter parsing
    In addition to just copying the cJSON to pass into the plugin, also add
    a typed parameters struct that will be passed into the plugins later in
    favor from the cJSON, to remove cJSON dependency from plugins.
    
    Signed-off-by: Yiyoung Liu <y-liu20@ti.com>

--- framework/core/config.h
+++ framework/core/config.h
@@ -1,13 +1,18 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include "am62d_plugin.h"
+
 #define MAX_NODES 16
 #define MAX_LINKS 32
+#define MAX_NODE_PARAMS 64
 
 struct node_config {
 	char id[64];
 	char plugin[64];
 	struct cJSON *params;
+	struct am62d_param typed_params[MAX_NODE_PARAMS];
+	int n_params;
 };
 
 struct link_config {
