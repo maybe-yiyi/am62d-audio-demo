maybe-yiyi/am62d-audio-demo

commit 0e2bcfebec0d87828a41acb5219af9a7facc03b3

a53_node: drive LV2 instance via lilv
Replace am62d_plugin port enumeration and process callback with lilv.

Also stubs out param_bus since lilv doesn't support control ports.

Signed-off-by: Yiyoung Liu <y-liu20@ti.com>

framework/core/a53_node.c
@@ -1,41 +1,27 @@
 #include <stdlib.h>
 
+#include <lv2/core/lv2.h>
 #include <pipewire/pipewire.h>
 
 #include "a53_node.h"
-#include "cJSON.h"
 #include "param_bus.h"
-
-static enum pw_direction to_pw_direction(enum am62d_port_dir dir)
-{
-	switch (dir) {
-	case AM62D_DIR_IN:
-		return PW_DIRECTION_INPUT;
-	case AM62D_DIR_OUT:
-		return PW_DIRECTION_OUTPUT;
-	default:
-		fprintf(stderr, "a53_node: unknown port direction %d\n", dir);
-		return PW_DIRECTION_INPUT;
-	}
-}
 
 static void on_process(void *data, struct spa_io_position *pos)
 {
 	struct a53_node *node = data;
-	const float *in[MAX_PORTS];
-	float *out[MAX_PORTS];
 	uint32_t n_frames = pos->clock.duration;
 
 	for (int i = 0; i < node->n_in; i++)
-		in[i] = pw_filter_get_dsp_buffer(node->in_ports[i], n_frames);
+		lilv_instance_connect_port(node->instance,
+				node->in_port_indices[i],
+				pw_filter_get_dsp_buffer(node->in_ports[i], n_frames));
+
 	for (int i = 0; i < node->n_out; i++)
-		out[i] = pw_filter_get_dsp_buffer(node->out_ports[i], n_frames);
+		lilv_instance_connect_port(node->instance,
+				node->out_port_indices[i],
+				pw_filter_get_dsp_buffer(node->out_ports[i], n_frames));
 
-	node->plugin->process(node->priv, in, out, n_frames,
-			(struct am62d_data_buf *const *)node->meta_in,
-			node->meta_out,
-			node->ctrl_out_vals);
-
+	lilv_instance_run(node->instance, n_frames);
 	param_bus_dispatch(node);
 }
 
@@ -45,96 +31,78 @@ static const struct pw_filter_events filter_events = {
 };
 
 struct a53_node *a53_node_create(struct pw_core *core,
-				 const struct am62d_plugin *plugin,
-				 const char *node_name,
-				 const struct am62d_param *params,
-				 int n_params)
+				 LilvWorld *world,
+				 const LilvPlugin *plugin,
+				 LilvInstance *instance,
+				 const char *node_name)
 {
+	LilvNode *audio_class = lilv_new_uri(world, LV2_CORE__AudioPort);
+	LilvNode *control_class = lilv_new_uri(world, LV2_CORE__ControlPort);
+	LilvNode *input_class = lilv_new_uri(world, LV2_CORE__InputPort);
+
 	struct a53_node *node = calloc(1, sizeof(struct a53_node));
 	if (!node)
 		goto exit;
 
 	node->plugin = plugin;
+	node->instance = instance;
 
-	int ret = plugin->init(&node->priv, params, n_params);
-	if (ret < 0)
-		goto free_node;
+	const char *plugin_uri = lilv_node_as_uri(lilv_plugin_get_uri(plugin));
 
-	node->filter = pw_filter_new(core, plugin->name,
+	node->filter = pw_filter_new(core, plugin_uri,
 		pw_properties_new(
 			PW_KEY_NODE_NAME, node_name,
 			PW_KEY_MEDIA_TYPE, "AUDIO",
 			PW_KEY_MEDIA_CATEGORY, "FILTER",
 			NULL));
 	if (!node->filter)
-		goto destroy_plugin;
+		goto free_node;
 
-	pw_filter_add_listener(node->filter,
-			&node->filter_listener,
-			&filter_events,
-			node);
+	pw_filter_add_listener(node->filter, &node->filter_listener,
+			&filter_events, node);
 
-	for (int i = 0; i < plugin->n_ports; i++) {
-		const struct am62d_port_desc *desc = &plugin->ports[i];
+	uint32_t n_ports = lilv_plugin_get_num_ports(plugin);
+	for (uint32_t i = 0; i < n_ports; i++) {
+		const LilvPort *port = lilv_plugin_get_port_by_index(plugin, i);
+		bool is_audio = lilv_port_is_a(plugin, port, audio_class);
+		bool is_control = lilv_port_is_a(plugin, port, control_class);
+		bool is_input = lilv_port_is_a(plugin, port, input_class);
+		const char *sym = lilv_node_as_string(lilv_port_get_symbol(plugin, port));
 
-		switch (desc->type) {
-		case AM62D_PORT_METADATA:
-			if (desc->dir != AM62D_DIR_OUT) {
-				if (node->n_meta_in >= MAX_PORTS)
-					goto destroy_filter;
-				node->n_meta_in++;
-				break;
-			}
-
-			if (node->n_meta_out >= MAX_PORTS)
+		if (is_audio) {
+			if (is_input && node->n_in >= MAX_PORTS)
 				goto destroy_filter;
-
-			size_t sz = sizeof(struct am62d_data_buf) + desc->u.meta.payload_size;
-
-			struct am62d_data_buf *buf = calloc(1, sz);
-			if (!buf)
+			if (!is_input && node->n_out >= MAX_PORTS)
 				goto destroy_filter;
-			buf->type_tag     = desc->u.meta.type_tag;
-			buf->payload_size = desc->u.meta.payload_size;
-
-			node->meta_out[node->n_meta_out++] = buf;
-			break;
-		case AM62D_PORT_CONTROL:
-			if (desc->dir == AM62D_DIR_OUT) {
-				if (node->n_ctrl_out >= MAX_PORTS)
-					goto destroy_filter;
-				node->n_ctrl_out++;
-			}
-			break;
-		default:
-			char dsp_format[64];
-			uint32_t ch = desc->u.pcm.n_channels;
-			if (ch == 1)
-				snprintf(dsp_format, sizeof(dsp_format), "32 bit float mono audio");
-			else
-				snprintf(dsp_format, sizeof(dsp_format), "32 bit %u channel audio", ch);
 
 			struct port_data *pd = pw_filter_add_port(node->filter,
-					to_pw_direction(desc->dir),
+					is_input ? PW_DIRECTION_INPUT : PW_DIRECTION_OUTPUT,
 					PW_FILTER_PORT_FLAG_MAP_BUFFERS,
 					sizeof(struct port_data),
 					pw_properties_new(
-						PW_KEY_FORMAT_DSP, dsp_format,
-						PW_KEY_PORT_NAME, desc->name,
+						PW_KEY_FORMAT_DSP, "32 bit float mono audio",
+						PW_KEY_PORT_NAME, sym,
 						NULL),
 					NULL, 0);
 			if (!pd)
 				goto destroy_filter;
 
-			struct port_data **port_arr = (desc->dir == AM62D_DIR_IN)
-				? node->in_ports : node->out_ports;
-			int *n = (desc->dir == AM62D_DIR_IN) ? &node->n_in : &node->n_out;
-
-			if (*n >= MAX_PORTS)
+			if (is_input) {
+				node->in_ports[node->n_in] = pd;
+				node->in_port_indices[node->n_in] = i;
+				node->n_in++;
+			} else {
+				node->out_ports[node->n_out] = pd;
+				node->out_port_indices[node->n_out] = i;
+				node->n_out++;
+			}
+		} else if (is_control) {
+			if (node->n_ctrl >= MAX_PORTS)
 				goto destroy_filter;
-
-			port_arr[*n] = pd;
-			(*n)++;
+			node->ctrl_bufs[node->n_ctrl] = 0.0f;
+			lilv_instance_connect_port(instance, i,
+					&node->ctrl_bufs[node->n_ctrl]);
+			node->n_ctrl++;
 		}
 	}
 
@@ -144,27 +112,27 @@ struct a53_node *a53_node_create(struct pw_core *core,
 	if (ret < 0)
 		goto destroy_filter;
 
+	lilv_instance_activate(instance);
+
+	lilv_node_free(audio_class);
+	lilv_node_free(control_class);
+	lilv_node_free(input_class);
 	return node;
 
 destroy_filter:
-	for (int i = 0; i < node->n_meta_out; i++)
-		free(node->meta_out[i]);
-
 	pw_filter_destroy(node->filter);
-destroy_plugin:
-	plugin->destroy(node->priv);
 free_node:
 	free(node);
 exit:
+	lilv_node_free(audio_class);
+	lilv_node_free(control_class);
+	lilv_node_free(input_class);
 	return NULL;
 }
 
 void a53_node_destroy(struct a53_node *node)
 {
-	for (int i = 0; i < node->n_meta_out; i++)
-		free(node->meta_out[i]);
-
+	lilv_instance_deactivate(node->instance);
 	pw_filter_destroy(node->filter);
-	node->plugin->destroy(node->priv);
 	free(node);
 }

framework/core/a53_node.h
@@ -2,48 +2,36 @@
 #define A53_NODE_H
 
 #include <pipewire/pipewire.h>
-
-#include "am62d_plugin.h"
+#include <lilv/lilv.h>
 
 #define MAX_PORTS 8
-#define MAX_CTRL_ROUTES 8
 
 struct port_data {
 	/* intentionally empty */
 };
 
-struct ctrl_route {
-	int ctrl_out_idx;
-	struct a53_node *target;
-	char param_key[64];
-};
-
 struct a53_node {
 	struct pw_filter *filter;
 	struct spa_hook filter_listener;
-	const struct am62d_plugin *plugin;
-	void *priv;
+	const LilvPlugin *plugin;
+	LilvInstance *instance;
+
 	struct port_data *in_ports[MAX_PORTS];
 	struct port_data *out_ports[MAX_PORTS];
+	uint32_t in_port_indices[MAX_PORTS];
+	uint32_t out_port_indices[MAX_PORTS];
 	int n_in;
 	int n_out;
 
-	struct am62d_data_buf *meta_in[MAX_PORTS];
-	struct am62d_data_buf *meta_out[MAX_PORTS];
-	int n_meta_in;
-	int n_meta_out;
-
-	struct ctrl_route ctrl_in_routes[MAX_CTRL_ROUTES];
-	float ctrl_out_vals[MAX_PORTS];
-	int n_ctrl_in;
-	int n_ctrl_out;
+	float ctrl_bufs[MAX_PORTS];
+	int n_ctrl;
 };
 
 struct a53_node *a53_node_create(struct pw_core *core,
-				 const struct am62d_plugin *plugin,
-				 const char *node_id,
-				 const struct am62d_param *params,
-				 int n_params);
+				 LilvWorld *world,
+				 const LilvPlugin *plugin,
+				 LilvInstance *instance,
+				 const char *node_name);
 void a53_node_destroy(struct a53_node *node);
 
 #endif

framework/core/param_bus.c
@@ -1,42 +1,16 @@
-#include <string.h>
+#include <stdio.h>
 
 #include "param_bus.h"
 
-static int ctrl_port_idx(const struct am62d_plugin *plugin, const char *name)
-{
-	int idx = 0;
-	for (int i = 0; i < plugin->n_ports; i++) {
-		const struct am62d_port_desc *pd = &plugin->ports[i];
-		if (pd->type != AM62D_PORT_CONTROL || pd->dir != AM62D_DIR_OUT)
-			continue;
-		if (strcmp(pd->name, name) == 0)
-			return idx;
-		idx++;
-	}
-	return -1;
-}
-
 void param_bus_dispatch(struct a53_node *node)
 {
-	int n = __atomic_load_n(&node->n_ctrl_in, __ATOMIC_ACQUIRE);
-	for (int i = 0; i < n; i++) {
-		struct ctrl_route *r = &node->ctrl_in_routes[i];
-		r->target->plugin->set_control(r->target->priv, r->param_key,
-					       node->ctrl_out_vals[r->ctrl_out_idx]);
-	}
+	(void)node;
 }
 
 int param_bus_register(struct a53_node *src, const char *port_name,
 		       struct a53_node *target, const char *param_key)
 {
-	int idx = ctrl_port_idx(src->plugin, port_name);
-	if (idx < 0)
-		return -1;
-
-	struct ctrl_route *r = &src->ctrl_in_routes[src->n_ctrl_in];
-	r->ctrl_out_idx = idx;
-	r->target = target;
-	snprintf(r->param_key, sizeof(r->param_key), "%s", param_key);
-	__atomic_store_n(&src->n_ctrl_in, src->n_ctrl_in + 1, __ATOMIC_RELEASE);
-	return 0;
+	(void)src; (void)port_name; (void)target; (void)param_key;
+	fprintf(stderr, "param_bus: control routing not yet implemented for LV2\n");
+	return -1;
 }

framework/core/pipeline.c
@@ -5,7 +5,6 @@
 #include <pipewire/pipewire.h>
 
 #include "a53_node.h"
-#include "am62d_plugin.h"
 #include "config.h"
 #include "param_bus.h"
 #include "pipeline.h"
@@ -49,29 +48,6 @@ static struct a53_node *pipeline_find_node(struct pipeline *pl, const char *conf
 	return NULL;
 }
 
-static int port_type(const struct a53_node *node, const char *port_name)
-{
-	for (int i = 0; i < node->plugin->n_ports; i++)
-		if (strcmp(node->plugin->ports[i].name, port_name) == 0)
-			return node->plugin->ports[i].type;
-	return -1;
-}
-
-static int meta_idx(const struct a53_node *node, const char *port_name,
-		    enum am62d_port_dir dir)
-{
-	int idx = 0;
-	for (int i = 0; i < node->plugin->n_ports; i++) {
-		const struct am62d_port_desc *pd = &node->plugin->ports[i];
-		if (pd->type != AM62D_PORT_METADATA || pd->dir != dir)
-			continue;
-		if (strcmp(pd->name, port_name) == 0)
-			return idx;
-		idx++;
-	}
-	return -1;
-}
-
 static void pipeline_wire_control_links(struct pipeline *pl)
 {
 	for (int i = 0; i < pl->config->n_ctrl_links; i++) {
@@ -102,39 +78,6 @@ static void pipeline_wire_control_links(struct pipeline *pl)
 	}
 }
 
-static void pipeline_wire_metadata(struct pipeline *pl)
-{
-	for (int i = 0; i < pl->config->n_links; i++) {
-		struct link_config *lc = &pl->config->links[i];
-
-		char src_node_id[64], src_port_name[64];
-		char dst_node_id[64], dst_port_name[64];
-		if (sscanf(lc->from, "%63[^:]:%63s", src_node_id, src_port_name) != 2 ||
-		    sscanf(lc->to,   "%63[^:]:%63s", dst_node_id, dst_port_name) != 2)
-			continue;
-
-		struct a53_node *src = pipeline_find_node(pl, src_node_id);
-		struct a53_node *dst = pipeline_find_node(pl, dst_node_id);
-		if (!src || !dst)
-			continue;
-
-		if (port_type(src, src_port_name) != AM62D_PORT_METADATA)
-			continue;
-
-		int src_idx = meta_idx(src, src_port_name, AM62D_DIR_OUT);
-		int dst_idx = meta_idx(dst, dst_port_name, AM62D_DIR_IN);
-
-		if (src_idx < 0 || dst_idx < 0) {
-			fprintf(stderr, "pipeline: metadata wire failed: %s -> %s\n",
-				lc->from, lc->to);
-			continue;
-		}
-
-		dst->meta_in[dst_idx] = src->meta_out[src_idx];
-		printf("Wired metadata %s -> %s\n", lc->from, lc->to);
-	}
-}
-
 static void pipeline_create_links(struct pipeline *pl)
 {
 	for (int i = 0; i < pl->config->n_links; i++) {
@@ -150,13 +93,6 @@ static void pipeline_create_links(struct pipeline *pl)
 			fprintf(stderr, "pipeline: malformed link '%s' -> '%s'\n",
 				link.from, link.to);
 			continue;
-		}
-
-		struct a53_node *src_node = pipeline_find_node(pl, from_node);
-		if (src_node) {
-			int type = port_type(src_node, from_port);
-			if (type != AM62D_PORT_AUDIO_PCM && type != AM62D_PORT_AUDIO_SPECTRUM)
-				continue;
 		}
 
 		uint32_t out_node = find_node_id(pl, from_node);
@@ -255,7 +191,6 @@ static void on_core_done(void *data, uint32_t id, int seq)
 			break;
 		case SYNC_PHASE_CREATE_LINKS:
 			pipeline_wire_control_links(pl);
-			pipeline_wire_metadata(pl);
 			pipeline_create_links(pl);
 			break;
 		default:
@@ -282,10 +217,11 @@ struct pipeline *pipeline_create(const char *config_path, const char *plugin_dir
 		struct node_config node_conf = pl->config->nodes[i];
 		printf("Loading node %s\n", node_conf.id);
 
-		const struct am62d_plugin *plugin = registry_get(node_conf.plugin);
+		LilvInstance *instance = registry_get(node_conf.plugin);
+		const LilvPlugin *plugin = registry_get_plugin(node_conf.plugin);
 
-		struct a53_node *a53_node = a53_node_create(pl->core, plugin, node_conf.id,
-						node_conf.typed_params, node_conf.n_params);
+		struct a53_node *a53_node = a53_node_create(pl->core, registry_world(),
+						plugin, instance, node_conf.id);
 		if (!a53_node)
 			return NULL;
 		pl->nodes[pl->n_nodes++] = a53_node;

framework/core/registry.c
@@ -33,6 +33,22 @@ int registry_init(const char *dir)
 	return 0;
 }
 
+LilvWorld *registry_world(void)
+{
+	return world;
+}
+
+const LilvPlugin *registry_get_plugin(const char *uri)
+{
+	for (int i = 0; i < cache_count; i++) {
+		const char *cached = lilv_node_as_uri(
+			lilv_plugin_get_uri(cache[i].plugin));
+		if (strcmp(cached, uri) == 0)
+			return cache[i].plugin;
+	}
+	return NULL;
+}
+
 LilvInstance *registry_get(const char *uri)
 {
 	for (int i = 0; i < cache_count; i++) {

framework/core/registry.h
@@ -4,7 +4,9 @@
 #include <lilv/lilv.h>
 
 int registry_init(const char *dir);
+LilvWorld *registry_world(void);
 LilvInstance *registry_get(const char *uri);
+const LilvPlugin *registry_get_plugin(const char *uri);
 void registry_destroy(void);
 
 #endif

commit 37b992477bcf5e49a071c05d0369231d01072337

a53_node: add initialization type to ret
Since Commit 0e2bcfe ("a53_node: drive LV2 instance via lilv") removes
the first declaration of int ret, we need to add it to this later
declaration so that the code will build.

Fixes: 0e2bcfe ("a53_node: drive LV2 instance via lilv")

Signed-off-by: Yiyoung Liu <y-liu20@ti.com>

framework/core/a53_node.c
@@ -106,7 +106,7 @@ struct a53_node *a53_node_create(struct pw_core *core,
 		}
 	}
 
-	ret = pw_filter_connect(node->filter,
+	int ret = pw_filter_connect(node->filter,
 			PW_FILTER_FLAG_RT_PROCESS,
 			NULL, 0);
 	if (ret < 0)